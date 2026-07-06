68831306f30240ab89de549905471c750ab91aec
vanessa2103206 <vanessac2016@libero.it>
2026-07-06 18:50:31 +0200

    Esercizio tabellina completo

---
+++ main.js
@@ -0,0 +1,9 @@
+let num = 2; // Il numero di partenza indicato dall'esercizio
+
+console.log(`Tabellina del ${num}:`);
+
+// Il ciclo parte da 1 e arriva fino a 10
+for (let i = 1; i <= 10; i++) {
+    let risultato = num * i;
+    console.log(`${num} x ${i} = ${risultato}`);
+}
